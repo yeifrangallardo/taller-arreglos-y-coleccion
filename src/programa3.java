be93333e737b0775e588d4310359e2f1fea16b3f
Yeifran gallardo <yeifrangallardo2524@gmail.com>
2024-05-31 00:50:05 -0500

    taller

---
+++ src/programa3.java
@@ -0,0 +1,18 @@
+public class Alumno
+ {
+ private String nombre;
+ private double nota;
+ public Alumno(String nombre,double nota)
+ {
+ this.nombre=nombre;
+ this.nota=nota;
+ }
+ public String getNombre()
+ {
+ return nombre;
+ }  
+public double getNota()
+ {
+ return nota;
+ }    
+}
