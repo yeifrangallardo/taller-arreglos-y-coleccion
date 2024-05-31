4628054c510bc47e1f844c384579418da071bd4e
Yeifran gallardo <yeifrangallardo2524@gmail.com>
2024-05-31 00:49:24 -0500

    taller

---
+++ src/programa2.java
@@ -0,0 +1,8 @@
+public class Programa3
+ {
+ public static void main(String arg[])
+ {
+ String nombre[]={"Juan","Pedro","Luis","Lina","Diana"};
+ for(int i=0;i<5;i++)
+ {
+ System.out.println(nombre[i])
